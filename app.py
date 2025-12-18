06f8f39c56ee665d47215cdaf524d3ef1cefcbe0
Pedro_US <158114855+Pedrin-US@users.noreply.github.com>
2025-12-18 13:26:30 -0300

    Update WhatsApp number and formatting in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("✂️ Bigode Barber")
 st.subheader("Agende seu horário com facilidade")
 
 # --- Dados da Barbearia (Edite aqui) ---
-SEU_NUMERO_WHATSAPP = "5571984290236" # Coloque seu número com DDD (ex: 55 + DDD + Numero)
+SEU_NUMERO_WHATSAPP = "5571999535533" # Coloque seu número com DDD (ex: 55 + DDD + Numero)
 HORARIOS = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30","09:00", "09:30", "10:00", "10:30", "11:00", "11:30","12:00", "12:30", "13:00", "13:30", "14:00", "14:30","15:00", "15:30", "16:00", "16:30", "17:00", "17:30","18:00", "18:30", "19:00", "19:30", "20:00", "20:30","21:00", "21:30", "22:00", "22:30", "23:00", "23:30"]
 SERVICOS = ["BARBA R$15", "CABELO R$25", "BARBA+CABELO R$35", "NEVOU R$100", "LUZES R$80", "PEZINHO R$10", "PIGMENTAÇÃO R$10"]
 
@@ -43,4 +43,5 @@ with st.container():
     else:
 
         st.warning("Por favor, preencha seu nome para liberar o botão de agendamento.")
+
 
